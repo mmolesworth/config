66d68c6f2c2eea0ce9d0acfdc1078094a51d6d4d
mmolesworth <m.molesworth@gmail.com>
2018-04-28 16:34:22 -0400

    Add files via upload

---
+++ development-setup.sh
@@ -0,0 +1,13 @@
+# install sudo
+# su root
+# apt-get install sudo
+# adduser <username> sudo
+# logout
+
+mkdir ~/software
+
+sudo apt-get install emacs git python-pip curl
+
+pip install awscli
+
+curl -0 ~/software/Anaconda3-5.1.0-Linux-x86_64.sh https://repo.anaconda.com/archive/Anaconda3-5.1.0-Linux-x86_64.sh
